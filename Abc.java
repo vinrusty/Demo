87bbc50b518517bebdc3dd146171ae817562f93d
vinrusty <vineethhr2016@gmail.com>
2024-02-29 10:13:33 +0530

    changes

--- Abc.java
+++ Abc.java
@@ -2,6 +2,6 @@ public class Abc{
     public static void main(String[] args) {
         System.out.println("Hello everybodyyy");
         System.out.println("Vin");
-        System.out.println("rusty");
+        System.out.println("rusty 1");
     }
 }
